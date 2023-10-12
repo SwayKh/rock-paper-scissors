795c2bf96d90ee01fab57ab53970cea18a451356
SwayKh <khatrisumit712@gmail.com>
2023-10-12 19:47:15 +0530

    Create UI for RPS
    Create UI, Added some CSS, Also added javascript
    event handler to buttons

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ function computerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
+function updateScore() {
+    pScore.textContent = `Player : ${playerScore}`;
+    cScore.textContent = `Computer : ${compScore}`;
+    tScore.textContent = `Ties : ${numberOfTie}`;
+}
+
 function playRound(playerChoice, compChoice) {
     let whoWins = "";
 
@@ -55,19 +61,55 @@ function playRound(playerChoice, compChoice) {
 
 function Game() {
 
-    for (let i = 0; i < 5; i++) {
-        let choiceOfComputer = computerChoice();
-        choiceOfPlayer = window.prompt("Enter a Choice[Rock, Paper, Scissors]", "paper")
-        console.log(playRound(choiceOfPlayer, choiceOfComputer));
-        console.log(`Player Score : ${playerScore} ComputerScore: ${compScore}`)
-    }
-    if (playerScore > compScore) {
-        alert(`You Win!\nPlayer : ${playerScore} Computer : ${compScore} Tie : ${numberOfTie}`);
-    } else if (playerScore < compScore) {
-        alert(`You Lose!\nPlayer : ${playerScore} Computer : ${compScore} Tie : ${numberOfTie}`);
-    } else {
-        alert(`Tie!\nPlayer : ${playerScore} Computer : ${compScore} Tie : ${numberOfTie}`);
-    }
+    // for (let i = 0; i < 5; i++) {
+    //     let choiceOfComputer = computerChoice();
+    //     choiceOfPlayer = window.prompt("Enter a Choice[Rock, Paper, Scissors]", "paper")
+    //     console.log(playRound(choiceOfPlayer, choiceOfComputer));
+    //     console.log(`Player Score : ${playerScore} ComputerScore: ${compScore}`)
+    // }
+    // if (playerScore > compScore) {
+    //     alert(`You Win!\nPlayer : ${playerScore} Computer : ${compScore} Tie : ${numberOfTie}`);
+    // } else if (playerScore < compScore) {
+    //     alert(`You Lose!\nPlayer : ${playerScore} Computer : ${compScore} Tie : ${numberOfTie}`);
+    // } else {
+    //     alert(`Tie!\nPlayer : ${playerScore} Computer : ${compScore} Tie : ${numberOfTie}`);
+    // }
 }
-
 Game();
+
+const scoreContainer = document.querySelector('.scoreContainer');
+const buttonContainer = document.querySelector('.buttonContainer');
+const winnerContainer = document.querySelector('.winnerContainer');
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const pScore = document.querySelector('#playerScore');
+const tScore = document.querySelector('#tieScore');
+const cScore = document.querySelector('#computerScore');
+
+
+rock.addEventListener('click', function (e) {
+    const div = document.createElement('div');
+    div.textContent = "This round: " + playRound(e.target.id, computerChoice())
+    winnerContainer.appendChild(div)
+    updateScore();
+})
+
+paper.addEventListener('click', function (e) {
+    const div = document.createElement('div');
+    div.textContent = "This round: " + playRound(e.target.id, computerChoice())
+    winnerContainer.appendChild(div)
+    updateScore();
+})
+
+scissors.addEventListener('click', function (e) {
+    const div = document.createElement('div');
+    div.textContent = "This round: " + playRound(e.target.id, computerChoice())
+    winnerContainer.appendChild(div)
+    updateScore();
+})
+
+
+
